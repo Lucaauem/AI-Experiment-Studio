0944316cf375bbb295e3dfb1e0ea2595e89af8ac
Lucaauem <luca-aussem@t-online.de>
2026-04-20 00:24:17 +0200

    [Frontend] Added: Navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
+import Navbar from "./components/Navbar"
+
 function App() {
   return (
-    <>
-      <p>Hello World!</p>
-    </>
+    <Navbar />
   )
 }
 
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <span className="nav-home">
+        <a href="">AI EXPERIMENT STUDIO</a>
+      </span>
+      <span className="nav-content">
+        <a href="">NAV 1</a>
+        <a href="">NAV 2</a>
+        <a href="">NAV 3</a>
+      </span>
+    </nav>
+  )
+}
